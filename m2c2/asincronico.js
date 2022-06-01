c4e8431fdbec256564fc790aa50a3d59fc12a77f
sebasolr <seba.olr@gmail.com>
2022-05-31 23:16:22 -0400

    modulo2class2

---
+++ m2c2/asincronico.js
@@ -0,0 +1,41 @@
+let variable = prompt("ingresa un numero:")
+if(variable % 2 == 0){alert("el numero es par")}
+else{(alert("el numero es impar"))};
+const jubilacionMujer = 60;
+const jubilacionHombre = 65;
+const hombre = 1;
+const mujer = 2;
+let nombre = prompt("Hola  cual es tu nombre:");
+let sexo = prompt("porfavor indica tu sexo (1) si eres hombre (2)si eres mujer.");
+let edadjubi = prompt("indica tu edad");
+let tiempoFaltanteM = (jubilacionMujer - edadjubi);
+let tiempoFaltanteH = (jubilacionHombre - edadjubi);
+// if(sexos == sexo && jubilacionMujer <= edadjubi){
+//     alert("Felicitaciones "+ nombre + " te puedes jubilar")
+// }
+// else if(sexos !== sexo && jubilacionHombre <= edadjubi){
+//     alert("felicidades "+ nombre +" puedes o estas jubilado")
+// }
+// else{
+//     alert("lo siento no puedes jubilarte aun")
+// };
+// if(sexos == sexo && jubilacionMujer > edadjubi){
+//     alert("te faltan "+ tiempoFaltanteM + "anos para jubilarte")
+// }
+// else if(sexos != sexo && edadjubi < jubilacionHombre){
+//     alert("te faltan " + tiempoFaltanteH + "anos para jubilarte")
+// }
+// else{"nothing"};
+
+if (mujer == sexo && edadjubi >= jubilacionMujer){
+    alert("gracias linda  por consultarnos, efectivamente puedes jubilarte")
+}
+else if(mujer == sexo && edadjubi < jubilacionMujer){
+    alert("lo siento linda aun no puedes jubilarte te faltan "+ tiempoFaltanteM + " anos para hacerlo")
+}
+else if(hombre == sexo && edadjubi >= jubilacionHombre){
+    alert("Felicitaciones estimado, puedes jubilarte")
+}
+else{
+    alert("lo siento estimado pero no puedes jubilarte, te faltan "+ tiempoFaltanteH + " anos para hacerlo")
+}
