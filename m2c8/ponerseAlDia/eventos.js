ed52ec10879c0c380b92cd525eaaa2bf8b8be531
sebasolr <seba.olr@gmail.com>
2022-06-08 16:56:04 -0400

    ponerme_al_dia

---
+++ m2c8/ponerseAlDia/eventos.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#boton");
+const fondo =document.querySelector("#contendeor");
+input.addEventListener('keyup', function(eve){
+    if (eve.keycode == 13){
+           console.log(eve)
+
+       }
+
+    
+})
+
